2712d33e6fa1af21b01e6316270f27b15c02d75a
Alexander Didenko <adidenko@mirantis.com>
2013-12-23 17:58:11 +0200

    Fixes

---
+++ environments/110_ubuntu_kvm_neutron_gre_sav_mur_cei_3nodes.py
@@ -0,0 +1,34 @@
+class environment:
+      data = {
+        "release": 3,
+        "mode": "multinode"
+      }
+      interfaces = {
+        'eth0': ["public"],
+        'eth1': ["private"],
+        'eth2': ["storage", "management"],
+        'eth3': ["fuelweb_admin"]
+      }
+      node_roles = [
+        ['controller'],
+        ['cinder'],
+        ['compute']
+      ]
+      net_tag = {
+         'management': 730,
+         'storage': 732,
+         'fixed': None
+      }
+      deploy_timeout = 120 * 60
+      settings = {
+        "murano": True,
+        "savanna": True,
+	"ceilometer": True,
+        "volumes_lvm": True,
+        "net_provider": 'neutron',
+        "net_segment_type": 'gre',
+        "libvirt_type": "kvm"
+      }
+      ostf_should_fail = 8
+      ostf_timeout = 6 * 60 * 60
+      ostf_test_sets = ['smoke', 'sanity', 'platform_tests']
---
+++ environments/120_ubuntu_ha_kvm_neutron_vlan_4nodes.py
@@ -0,0 +1,35 @@
+class environment:
+      data = {
+        "release": 3,
+        "mode": "ha_compact"
+      }
+      interfaces = {
+        'eth0': ["public"],
+        'eth1': ["private"],
+        'eth2': ["storage", "management"],
+        'eth3': ["fuelweb_admin"]
+      }
+      node_roles = [
+        ['controller', 'ceph-osd'],
+        ['controller', 'ceph-osd'],
+        ['controller', 'ceph-osd'],
+        ['compute', 'ceph-osd']
+      ]
+      net_tag = {
+         'management': 730,
+         'storage': 732,
+         'fixed': None
+      }
+      deploy_timeout = 180 * 60
+      settings = {
+        "volumes_ceph": True,
+        "images_ceph": True,
+        "volumes_lvm": False,
+        "net_provider": 'neutron',
+        "net_segment_type": 'vlan',
+        "neutron_vlan_range": [ 1000, 1009 ],
+        "libvirt_type": "kvm"
+      }
+      ostf_should_fail = 0
+      ostf_timeout = 6 * 60 * 60
+      ostf_test_sets = ['ha', 'smoke', 'sanity', 'platform_tests']
